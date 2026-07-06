553d0c918b09b0bc5769ac6219d4ce94412b10fd
samson-peters-dev <samson.peters85@gmail.com>
2026-07-06 10:20:27 +0100

    update

--- elixir/settings.py
+++ elixir/settings.py
@@ -130,8 +130,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/static/'
+STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = BASE_DIR / "staticfiles"
